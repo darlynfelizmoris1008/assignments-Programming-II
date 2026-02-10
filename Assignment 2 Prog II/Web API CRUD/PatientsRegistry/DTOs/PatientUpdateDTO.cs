03626bea1f3d028af062b2442f97e9720d87f007
Darlyn. <darlyngamer06@gmail.com>
2026-02-10 02:47:04 -0400

    Add Patient DTOs

---
+++ Assignment 2 Prog II/Web API CRUD/PatientsRegistry/DTOs/PatientUpdateDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace PatientsRegistry.DTOs
+{
+    public class PatientUpdateDTO
+    {
+        public string FullName { get; set; }
+        public string NationalId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmergencyContactName { get; set; }
+        public string EmergencyContactPhone { get; set; }
+        public int Age { get; set; }
+    }
+}
