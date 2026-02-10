87de93001aa1786e9e4a69b9d439bd2b84031add
Darlyn. <darlyngamer06@gmail.com>
2026-02-10 03:05:20 -0400

    Add Update and Delete methods to PatientRepository

--- Assignment 2 Prog II/Web API CRUD/PatientsRegistry/PatientRepository.cs
+++ Assignment 2 Prog II/Web API CRUD/PatientsRegistry/PatientRepository.cs
@@ -65,5 +65,50 @@ namespace PatientsRegistry
 
             return patients;
         }
+
+        public void UpdatePatient(int id, PatientUpdateDTO patient)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = @"
+            UPDATE Patients
+            SET 
+                FullName = @FullName,
+                NationalId = @NationalId,
+                PhoneNumber = @PhoneNumber,
+                EmergencyContactName = @EmergencyContactName,
+                EmergencyContactPhone = @EmergencyContactPhone,
+                Age = @Age
+            WHERE Id = @Id;
+        ";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Id", id);
+                command.Parameters.AddWithValue("@FullName", patient.FullName);
+                command.Parameters.AddWithValue("@NationalId", patient.NationalId);
+                command.Parameters.AddWithValue("@PhoneNumber", patient.PhoneNumber);
+                command.Parameters.AddWithValue("@EmergencyContactName", patient.EmergencyContactName);
+                command.Parameters.AddWithValue("@EmergencyContactPhone", patient.EmergencyContactPhone);
+                command.Parameters.AddWithValue("@Age", patient.Age);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeletePatient(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Patients WHERE Id = @Id";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Id", id);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
     }
 }
