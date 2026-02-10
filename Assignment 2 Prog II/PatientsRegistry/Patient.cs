5518a420dacdeff0954877c848117aca135c2e8b
Darlyn. <darlyngamer06@gmail.com>
2026-02-10 00:16:07 -0400

    Implement initial Patients CRUD (console-based) (Issue #2)

---
+++ Assignment 2 Prog II/PatientsRegistry/Patient.cs
@@ -0,0 +1,36 @@
+﻿namespace ManusHospital
+{
+    public class Patient
+    {
+        public int Id { get; private set; }
+        public string FullName { get; set; } = "";
+        public string NationalId { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+        public string EmergencyContactName { get; set; } = "";
+        public string EmergencyContactPhone { get; set; } = "";
+        public int Age { get; set; }
+
+        public Patient(
+            int id,
+            string fullName,
+            string nationalId,
+            int age,
+            string phone,
+            string emergencyName,
+            string emergencyPhone)
+        {
+            Id = id;
+            FullName = InputValidator.VerifyLettersOnly(fullName);
+            NationalId = InputValidator.VerifyCedula(nationalId);
+            Age = age;
+            PhoneNumber = InputValidator.VerifyPhone(phone);
+            EmergencyContactName = InputValidator.VerifyLettersOnly(emergencyName);
+            EmergencyContactPhone = InputValidator.VerifyPhone(emergencyPhone);
+        }
+
+        internal void SetId(int newId)
+        {
+            Id = newId;
+        }
+    }
+}
---
+++ Assignment 2 Prog II/PatientsRegistry/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ManusHospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PatientsApp.Run();
+        }
+    }
+}
