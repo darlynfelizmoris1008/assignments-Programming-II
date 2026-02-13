d72ce71fc508151ebb03ecaf60a8dc67c32fcc5d
Darlyn. <darlyngamer06@gmail.com>
2026-02-13 17:47:04 -0400

    Refine PatientRepository methods and ensure full CRUD database integration

--- Assignment 2 Prog II/Web API CRUD/PatientsRegistry/PatientRepository.cs
+++ Assignment 2 Prog II/Web API CRUD/PatientsRegistry/PatientRepository.cs
@@ -1,66 +1,65 @@
-﻿using System.Collections.Generic;
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
 using PatientsRegistry.DTOs;
 
 namespace PatientsRegistry
 {
-    internal class PatientRepository
+    public class PatientRepository
     {
-        private readonly string connectionString =
-            "Server=LEANDRO\\SQLEXPRESS;Database=ManusHospitalDB;Trusted_Connection=True;TrustServerCertificate=True;";
+        private readonly string _connectionString;
+
+        public PatientRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
 
         public void AddPatient(PatientCreateDTO patient)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                string query = @"
-                    INSERT INTO Patients
-                    (FullName, NationalId, PhoneNumber, EmergencyContactName, EmergencyContactPhone, Age)
-                    VALUES
-                    (@FullName, @NationalId, @PhoneNumber, @EmergencyContactName, @EmergencyContactPhone, @Age);
-                ";
+            using SqlConnection connection = new SqlConnection(_connectionString);
 
-                SqlCommand command = new SqlCommand(query, connection);
+            string query = @"
+                INSERT INTO Patients
+                (FullName, NationalId, PhoneNumber, EmergencyContactName, EmergencyContactPhone, Age)
+                VALUES
+                (@FullName, @NationalId, @PhoneNumber, @EmergencyContactName, @EmergencyContactPhone, @Age);
+            ";
 
-                command.Parameters.AddWithValue("@FullName", patient.FullName);
-                command.Parameters.AddWithValue("@NationalId", patient.NationalId);
-                command.Parameters.AddWithValue("@PhoneNumber", patient.PhoneNumber);
-                command.Parameters.AddWithValue("@EmergencyContactName", patient.EmergencyContactName);
-                command.Parameters.AddWithValue("@EmergencyContactPhone", patient.EmergencyContactPhone);
-                command.Parameters.AddWithValue("@Age", patient.Age);
+            SqlCommand command = new SqlCommand(query, connection);
 
-                connection.Open();
-                command.ExecuteNonQuery();
-            }
+            command.Parameters.AddWithValue("@FullName", patient.FullName);
+            command.Parameters.AddWithValue("@NationalId", patient.NationalId);
+            command.Parameters.AddWithValue("@PhoneNumber", patient.PhoneNumber);
+            command.Parameters.AddWithValue("@EmergencyContactName", patient.EmergencyContactName);
+            command.Parameters.AddWithValue("@EmergencyContactPhone", patient.EmergencyContactPhone);
+            command.Parameters.AddWithValue("@Age", patient.Age);
+
+            connection.Open();
+            command.ExecuteNonQuery();
         }
 
         public List<PatientReadDTO> GetAllPatients()
         {
-            List<PatientReadDTO> patients = new List<PatientReadDTO>();
+            List<PatientReadDTO> patients = new();
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using SqlConnection connection = new SqlConnection(_connectionString);
+
+            string query = "SELECT * FROM Patients";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
             {
-                string query = "SELECT * FROM Patients";
-                SqlCommand command = new SqlCommand(query, connection);
-
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
+                patients.Add(new PatientReadDTO
                 {
-                    PatientReadDTO patient = new PatientReadDTO
-                    {
-                        Id = (int)reader["Id"],
-                        FullName = reader["FullName"].ToString()!,
-                        NationalId = reader["NationalId"].ToString()!,
-                        PhoneNumber = reader["PhoneNumber"].ToString()!,
-                        EmergencyContactName = reader["EmergencyContactName"].ToString()!,
-                        EmergencyContactPhone = reader["EmergencyContactPhone"].ToString()!,
-                        Age = (int)reader["Age"]
-                    };
-
-                    patients.Add(patient);
-                }
+                    Id = (int)reader["Id"],
+                    FullName = reader["FullName"].ToString()!,
+                    NationalId = reader["NationalId"].ToString()!,
+                    PhoneNumber = reader["PhoneNumber"].ToString()!,
+                    EmergencyContactName = reader["EmergencyContactName"].ToString()!,
+                    EmergencyContactPhone = reader["EmergencyContactPhone"].ToString()!,
+                    Age = (int)reader["Age"]
+                });
             }
 
             return patients;
@@ -68,47 +67,45 @@ namespace PatientsRegistry
 
         public void UpdatePatient(int id, PatientUpdateDTO patient)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                string query = @"
-            UPDATE Patients
-            SET 
-                FullName = @FullName,
-                NationalId = @NationalId,
-                PhoneNumber = @PhoneNumber,
-                EmergencyContactName = @EmergencyContactName,
-                EmergencyContactPhone = @EmergencyContactPhone,
-                Age = @Age
-            WHERE Id = @Id;
-        ";
+            using SqlConnection connection = new SqlConnection(_connectionString);
 
-                SqlCommand command = new SqlCommand(query, connection);
+            string query = @"
+                UPDATE Patients
+                SET 
+                    FullName = @FullName,
+                    NationalId = @NationalId,
+                    PhoneNumber = @PhoneNumber,
+                    EmergencyContactName = @EmergencyContactName,
+                    EmergencyContactPhone = @EmergencyContactPhone,
+                    Age = @Age
+                WHERE Id = @Id;
+            ";
 
-                command.Parameters.AddWithValue("@Id", id);
-                command.Parameters.AddWithValue("@FullName", patient.FullName);
-                command.Parameters.AddWithValue("@NationalId", patient.NationalId);
-                command.Parameters.AddWithValue("@PhoneNumber", patient.PhoneNumber);
-                command.Parameters.AddWithValue("@EmergencyContactName", patient.EmergencyContactName);
-                command.Parameters.AddWithValue("@EmergencyContactPhone", patient.EmergencyContactPhone);
-                command.Parameters.AddWithValue("@Age", patient.Age);
+            SqlCommand command = new SqlCommand(query, connection);
 
-                connection.Open();
-                command.ExecuteNonQuery();
-            }
+            command.Parameters.AddWithValue("@Id", id);
+            command.Parameters.AddWithValue("@FullName", patient.FullName);
+            command.Parameters.AddWithValue("@NationalId", patient.NationalId);
+            command.Parameters.AddWithValue("@PhoneNumber", patient.PhoneNumber);
+            command.Parameters.AddWithValue("@EmergencyContactName", patient.EmergencyContactName);
+            command.Parameters.AddWithValue("@EmergencyContactPhone", patient.EmergencyContactPhone);
+            command.Parameters.AddWithValue("@Age", patient.Age);
+
+            connection.Open();
+            command.ExecuteNonQuery();
         }
 
         public void DeletePatient(int id)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                string query = "DELETE FROM Patients WHERE Id = @Id";
+            using SqlConnection connection = new SqlConnection(_connectionString);
 
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@Id", id);
+            string query = "DELETE FROM Patients WHERE Id = @Id";
 
-                connection.Open();
-                command.ExecuteNonQuery();
-            }
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@Id", id);
+
+            connection.Open();
+            command.ExecuteNonQuery();
         }
     }
 }
